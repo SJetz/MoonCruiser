437ae3be6f215f77db912316c43cc13a23b660e1
SJetz <sarah.jetz@gmail.com>
2021-08-24 21:06:00 +0200

    Shader

---
+++ assets/shaders/skybox_frag.glsl
@@ -0,0 +1,116 @@
+#version 330 core
+
+//input from vertex shader
+in struct VertexData
+{
+    vec2 textureCoordinate;
+    vec3 normal;
+//light stuff
+    vec3 todirection;
+    float tointensity;
+    vec4 todiffuse;
+    vec4 tocolor;
+    vec4 toambientcolor;
+    vec4 tospecularcolor;
+    float toglossiness;
+    vec4 torimcolor;
+    float torimamount;
+    float torimthreshhold;
+//light stuff
+    vec3 toCamera;
+    vec3 toBikeSpotLight;
+    vec3 toBikeSpotLight2;
+} vertexData;
+
+uniform sampler2D materialDiff;
+uniform sampler2D materialSpec;
+uniform sampler2D materialEmit;
+uniform float materialShininess;
+
+uniform vec3 shadingColor = vec3(1f,1f,1f);
+
+uniform vec2 bikeSpotLightCone;
+uniform vec2 bikeSpotLight2Cone;
+
+uniform vec3 bikeSpotLightColor;
+uniform vec3 bikeSpotLight2Color;
+
+uniform vec3 bikeSpotLightDirection;
+uniform vec3 bikeSpotLight2Direction;
+
+uniform vec3 bikeSpotLightAttParams;
+uniform vec3 bikeSpotLight2AttParams;
+
+//fragment shader output
+out vec4 color;
+
+
+float getAttenuationFactor(float distance, vec3 attParams){
+    return 1.0f / (attParams.x + attParams.y * distance + attParams.z * distance * distance);
+}
+vec3 getSpotLightIntensity(vec3 color, vec3 toLightVector, vec3 lightdir, vec2 cone)
+{
+    float lth = length(toLightVector);
+    float cosfpos = dot(lightdir, normalize(toLightVector));
+    float att = clamp((cosfpos - cos(cone.y)) / (cos(cone.x) - cos(cone.y)), 0.0f, 1.0f);
+    return color * att * getAttenuationFactor(lth, bikeSpotLightAttParams);
+}
+
+vec3 shade(vec3 N, vec3 L, vec3 V, vec3 diffc, vec3 specc, float shn)
+{
+    vec3 R = normalize(reflect(-L, N));
+    float NdotL = max(dot(N, L), 0.0f);
+    float RdotV = max(dot(R, V), 0.0f);
+    return diffc * NdotL + specc * pow(RdotV, shn);
+}
+
+vec3 shadeBlinn(vec3 N, vec3 L, vec3 V, vec3 diffc, vec3 specc, float shn)
+{
+    vec3 H = normalize(V + L);
+    float NdotL = max(dot(N, L), 0.0f);
+    float HdotN = max(dot(H, N), 0.0f);
+    return diffc * NdotL + specc * pow(HdotN, shn);
+}
+
+
+void main(){
+
+    vec3 N = normalize(vertexData.normal);
+
+    vec3 Lbsl = normalize(vertexData.toBikeSpotLight);
+    vec3 Lbsl2 = normalize(vertexData.toBikeSpotLight2);
+    vec3 V = normalize(vertexData.toCamera);
+
+    vec3 diffColor = texture(materialDiff, vertexData.textureCoordinate).rgb;
+    vec3 specColor = texture(materialSpec, vertexData.textureCoordinate).rgb;
+    vec3 emitColor = texture(materialEmit, vertexData.textureCoordinate).rgb;
+
+    vec3 emit_term = emitColor * shadingColor;
+
+    vec3 sshade =  shade(N, Lbsl, V, diffColor, specColor, materialShininess);
+    vec3 sshade2 =  shade(N, Lbsl2, V, diffColor, specColor, materialShininess);
+
+    vec3 intSpotLight = getSpotLightIntensity(bikeSpotLightColor, vertexData.toBikeSpotLight, bikeSpotLightDirection, bikeSpotLightCone);
+    vec3 intSpotLight2 = getSpotLightIntensity(bikeSpotLight2Color, vertexData.toBikeSpotLight2, bikeSpotLight2Direction, bikeSpotLight2Cone);
+
+    color = vec4(emit_term +
+    sshade * intSpotLight +
+    sshade2 * intSpotLight2, 1.0f);
+
+
+    // Blinn-Phong shading:
+
+    sshade =  shadeBlinn(N, Lbsl, V, diffColor, specColor, materialShininess);
+    sshade2 =  shadeBlinn(N, Lbsl2, V, diffColor, specColor, materialShininess);
+
+    vec4 precolor = vec4(emit_term +
+    sshade * intSpotLight +
+    sshade2 * intSpotLight2, 1.0f);
+
+
+
+   color = (vertexData.tocolor + vertexData.toambientcolor ) * vertexData.tocolor * precolor;
+
+
+
+}
---
+++ assets/shaders/skybox_vert.glsl
@@ -0,0 +1,73 @@
+#version 330 core
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec2 textureCoordinate;
+layout(location = 2) in vec3 normal;
+
+//uniforms
+uniform mat4 model_matrix;
+uniform mat4 view_matrix;
+uniform mat4 proj_matrix;
+
+uniform vec2 tcMultiplier;
+
+// lights
+uniform vec3 bikeSpotLightPosition;
+uniform vec3 bikeSpotLight2Position;
+
+uniform vec3  direction = vec3(0.2f,-0.5f,-1.0f);
+uniform float intensity = 1;
+uniform vec4 diffuse = vec4(1f,1f,1f,1f);
+uniform vec4 color = vec4(1f,1f,1f,1f);
+uniform vec4 ambientcolor = vec4(0.4f,0.4f,0.4f,1f);
+uniform vec4 specularcolor = vec4(0.75f,0.75f,0.75f,1f);
+uniform float glossines = 32f;
+uniform vec4 rimcolor = vec4(1f,1f,1f,1f);
+uniform float rimamount = 0.716f;
+uniform float rimthreshhold = 0.1f;
+
+out struct VertexData
+{
+    vec2 textureCoordinate;
+    vec3 normal;
+//light
+    vec3 todirection;
+    float tointensity;
+    vec4 todiffuse;
+    vec4 tocolor;
+    vec4 toambientcolor;
+    vec4 tospecularcolor;
+    float toglossiness;
+    vec4 torimcolor;
+    float torimamount;
+    float torimthreshhold;
+//light stuff
+    vec3 toCamera;
+    vec3 toBikeSpotLight;
+    vec3 toBikeSpotLight2;
+} vertexData;
+
+void main(){
+    mat4 modelview = view_matrix * model_matrix;
+    vec4 viewpos = modelview * vec4(position, 1.0f);
+    vertexData.toCamera = -viewpos.xyz;
+
+    vertexData.toBikeSpotLight = (view_matrix * vec4(bikeSpotLightPosition, 1.0)).xyz - viewpos.xyz;
+    vertexData.toBikeSpotLight2 = (view_matrix * vec4(bikeSpotLight2Position, 1.0)).xyz - viewpos.xyz;
+
+    gl_Position = proj_matrix * viewpos;
+
+    vertexData.normal = (inverse(transpose(modelview)) * vec4(normal, 0.0f)).xyz;
+    vertexData.textureCoordinate = textureCoordinate * tcMultiplier;
+
+    vertexData.todirection = direction;
+    vertexData.tointensity = intensity;
+    vertexData.todiffuse = diffuse;
+    vertexData.tocolor = color;
+    vertexData.toambientcolor = ambientcolor;
+    vertexData.tospecularcolor = specularcolor;
+    vertexData.toglossiness = glossines;
+    vertexData.torimcolor = rimcolor;
+    vertexData.torimamount = rimamount;
+    vertexData.torimthreshhold = rimthreshhold;
+}
